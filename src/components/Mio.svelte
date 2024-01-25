18b5e09f5c102eb56ed401ce47c82fbfa5b6bd3d
Manuel Herdez <herdez@gmail.com>
2024-01-24 22:21:49 -0600

    configuración inicial Prettier (Astro, Svelte, React, Ts)

---
+++ src/components/Mio.svelte
@@ -0,0 +1,11 @@
+<script>
+  const frutas = ['manzana', 'pera', 'uva']
+
+  const agregarFruta = () => {
+    frutas.push('naranja')
+  }
+</script>
+
+<main>
+  <img src="..." alt="" />
+</main>
---
+++ src/components/Mio.tsx
@@ -0,0 +1,8 @@
+export const Mio = (): JSX.Element => {
+  return (
+    <>
+      <h1 className='clases y mas'>hola mundo</h1>
+      <h1 className='clases y mas'>hola mundo</h1>
+    </>
+  )
+}
